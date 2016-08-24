533aeac324041c59181b2ee11e6c82f78aa2b845
Cristian Conache <cristian.conache@devpadawan.ro>
2016-08-24 20:16:55 +0000

    Bootstraped results list

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,13 @@
 class CustomersController < ApplicationController
+	PAGE_SIZE = 10
   def index
+  	@page_size = PAGE_SIZE
   	@customers = []
+  	@page = params[:page].to_i > 0 ? params[:page].to_i : 0
     if params[:keywords].present? 
     	@keywords = params[:keywords]
     	customer_search_term = CustomerSearchTerm.new( @keywords )
-    	@customers = Customer.where(customer_search_term.where_clause,customer_search_term.where_args).order(customer_search_term.order)
+    	@customers = Customer.where(customer_search_term.where_clause,customer_search_term.where_args).order(customer_search_term.order).offset(PAGE_SIZE*@page).limit(PAGE_SIZE)
     end
   end
 end
